f46b98a08e36a1c540611122893854edd91cb2d7
lopesmarcojr <marcoaurelio.lopes.junior@gmail.com>
2022-07-11 18:14:40 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
-import NotificationButton from "./components/NotificationButton"
-import icon from "../../frontend/src/assets/notification-icon.svg"
+import SalesCard from "./components/SalesCard"
+import Header from "./components/Header"
 
 
 function App() {
   return (
     <>
-    <h1>OLá,mundo!</h1>
-    <div className="dsmeta-red-btn">
-      <img src={icon} alt="Notificar" />
-    </div>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
     </>
   )
 }
