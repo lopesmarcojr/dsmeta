244c543736e82a9e10762a70fb3b14c1dd8f46f2
lopesmarcojr <marcoaurelio.lopes.junior@gmail.com>
2022-07-11 16:46:31 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import NotificationButton from "./components/NotificationButton"
+import icon from "../../frontend/src/assets/notification-icon.svg"
+
+
 function App() {
-  return(
-    <h1>Olá,mundo</h1>
+  return (
+    <>
+    <h1>OLá,mundo!</h1>
+    <div className="dsmeta-red-btn">
+      <img src={icon} alt="Notificar" />
+    </div>
+    </>
   )
 }
 
---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+function NotificationButton() {
+    return(
+      <p>Meu componente</p>
+    )
+  }
+  
+  export default NotificationButton
+  
